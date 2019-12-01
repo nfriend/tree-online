25ae98f861c6d3261575e4686af81f8f36cc7a79
Nathan Friend <nathan@gitlab.com>
2019-12-01 12:37:10 -0400

    Add SCSS support

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import logo from './logo.svg';
 
 const App: React.FC = () => {
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import './index.scss';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
