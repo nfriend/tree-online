0eb17e371403f8c61c7e7e0a7f7f001f5abf48d1
Nathan Friend <nathan@gitlab.com>
2019-12-01 14:05:34 -0400

    Add dark/light theme support

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
 import React from 'react';
-import './App.scss';
-import logo from './logo.svg';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App p-4">
+      <h1>Hello, world!</h1>
+      <p>Welcome to tree.nathanfriend.io</p>
     </div>
   );
 };
