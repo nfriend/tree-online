ee66ea559ab15d7cb80ff56bcf74f1a3073b89e8
Nathan Friend <nathan@gitlab.com>
2019-12-01 22:04:50 -0400

    Add input parsing

---
+++ src/lib/generate-tree.ts
@@ -0,0 +1,9 @@
+import { FileStructure } from './FileStructure';
+
+/**
+ * Generates an ASCII tree diagram, given a FileStructure
+ * @param structure The FileStructure object to convert into ASCII
+ */
+export const generateTree = (structure: FileStructure): string => {
+  throw new Error('Not yet implemented!');
+};
---
+++ src/lib/parse-input.test.ts
@@ -0,0 +1,165 @@
+import { FileStructure } from './FileStructure';
+import { parseInput, splitInput } from './parse-input';
+
+// not using a template string in order
+// to have more control over whitespace
+const mockInput = [
+  'my-app',
+  '  src',
+  '    index.html',
+  '    main.ts',
+  '   main.scss',
+  '  - build',
+  '    - index.html',
+  '    main.js',
+  '    main.css',
+  '',
+  '  ',
+  '  .prettierrc.json',
+  '  .gitlab-ci.yml',
+  '  README.md',
+  'empty-dir',
+].join('\n');
+
+describe('parse-input', () => {
+  it('parses plain text input into a FileStructure object', () => {
+    const actual = parseInput(mockInput);
+
+    const root: FileStructure = {
+      name: 'root',
+      children: [],
+      indentCount: -1,
+      parent: null,
+    };
+
+    const myApp: FileStructure = {
+      name: 'my-app',
+      children: [],
+      indentCount: 0,
+      parent: root,
+    };
+    root.children.push(myApp);
+
+    const src: FileStructure = {
+      name: 'src',
+      children: [],
+      indentCount: 2,
+      parent: myApp,
+    };
+    myApp.children.push(src);
+
+    const srcIndexHtml: FileStructure = {
+      name: 'index.html',
+      children: [],
+      indentCount: 4,
+      parent: src,
+    };
+    src.children.push(srcIndexHtml);
+
+    const mainTs: FileStructure = {
+      name: 'main.ts',
+      children: [],
+      indentCount: 4,
+      parent: src,
+    };
+    src.children.push(mainTs);
+
+    const mainScss: FileStructure = {
+      name: 'main.scss',
+      children: [],
+      indentCount: 3,
+      parent: src,
+    };
+    src.children.push(mainScss);
+
+    const build: FileStructure = {
+      name: 'build',
+      children: [],
+      indentCount: 2,
+      parent: myApp,
+    };
+    myApp.children.push(build);
+
+    const buildIndexHtml: FileStructure = {
+      name: 'index.html',
+      children: [],
+      indentCount: 4,
+      parent: build,
+    };
+    build.children.push(buildIndexHtml);
+
+    const mainJs: FileStructure = {
+      name: 'main.js',
+      children: [],
+      indentCount: 4,
+      parent: build,
+    };
+    build.children.push(mainJs);
+
+    const mainCss: FileStructure = {
+      name: 'main.css',
+      children: [],
+      indentCount: 4,
+      parent: build,
+    };
+    build.children.push(mainCss);
+
+    const prettierRcJson: FileStructure = {
+      name: '.prettierrc.json',
+      children: [],
+      indentCount: 2,
+      parent: myApp,
+    };
+    myApp.children.push(prettierRcJson);
+
+    const gitlabCiYml: FileStructure = {
+      name: '.gitlab-ci.yml',
+      children: [],
+      indentCount: 2,
+      parent: myApp,
+    };
+    myApp.children.push(gitlabCiYml);
+
+    const readmeMd: FileStructure = {
+      name: 'README.md',
+      children: [],
+      indentCount: 2,
+      parent: myApp,
+    };
+    myApp.children.push(readmeMd);
+
+    const emptyDir: FileStructure = {
+      name: 'empty-dir',
+      children: [],
+      indentCount: 0,
+      parent: root,
+    };
+    root.children.push(emptyDir);
+
+    expect(actual).toEqual(root);
+  });
+});
+
+describe('splitInput', () => {
+  it('splits plain text into an array of File Structure objects', () => {
+    const actual = splitInput(mockInput);
+
+    const expected = [
+      { name: 'my-app', children: [], indentCount: 0, parent: null },
+      { name: 'src', children: [], indentCount: 2, parent: null },
+      { name: 'index.html', children: [], indentCount: 4, parent: null },
+      { name: 'main.ts', children: [], indentCount: 4, parent: null },
+      { name: 'main.scss', children: [], indentCount: 3, parent: null },
+      { name: 'build', children: [], indentCount: 2, parent: null },
+      { name: 'index.html', children: [], indentCount: 4, parent: null },
+      { name: 'main.js', children: [], indentCount: 4, parent: null },
+      { name: 'main.css', children: [], indentCount: 4, parent: null },
+      { name: '.prettierrc.json', children: [], indentCount: 2, parent: null },
+      { name: '.gitlab-ci.yml', children: [], indentCount: 2, parent: null },
+      { name: 'README.md', children: [], indentCount: 2, parent: null },
+      { name: 'empty-dir', children: [], indentCount: 0, parent: null },
+    ];
+
+    expect(actual).toEqual(expected);
+  });
+});
