d48a997cd3b1c0f681b1189ce96a027a3744f48a
Nathan Friend <nathan@gitlab.com>
2019-12-02 09:41:29 -0400

    Add under construction message

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,20 @@ const App: React.FC = () => {
   return (
     <div className="App p-4">
       <h1>Hello, world!</h1>
-      <p>Welcome to tree.nathanfriend.io</p>
+      <p>
+        Welcome to <b>tree.nathanfriend.io!</b>
+      </p>
+      <p>
+        This site is currently under construction. In the meantime, feel free to
+        check out the source at{' '}
+        <a href="https://gitlab.com/nfriend/tree-online">GitLab</a>!
+      </p>
+      <a href="https://gitlab.com/nfriend/tree-online/pipelines">
+        <img
+          src="https://gitlab.com/nfriend/tree-online/badges/master/pipeline.svg"
+          alt="GitLab build status"
+        />
+      </a>
     </div>
   );
 };
