cfc7ce6557fb7aac169b7e8999a71da3b0f85955
Nathan Friend <nathan@gitlab.com>
2019-12-03 11:13:58 -0400

    Fix deployment date replacement

--- ci/pages-preparation.sh
+++ ci/pages-preparation.sh
@@ -6,8 +6,11 @@ rm -rf public
 # Copy the "build" directory to "public" for GitLab Pages
 cp -r build public
 
+# The text in the build output that will be replaced with the timestamp
+replacement='%%%GITLAB_CI_TIMESTAMP%%%'
+
 # Get a timestamp that looks like 2019-12-03T13:08:42Z
 timestamp=`date -u +'%Y-%m-%dT%H:%M:%SZ'`
 
 # Stamp the build output with the current timestamp
-sed -i'.original' -e "s/%%%GITLAB_CI_TIMESTAMP%%%/$timestamp/g" public/*.js
+grep -rl $replacement public | xargs sed -i'.original' -e "s/$replacement/$timestamp/g"
