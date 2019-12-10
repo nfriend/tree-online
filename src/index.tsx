141e1a263ebb2c8b1c3d58ca7698561dad50e58a
Nathan Friend <nathan@gitlab.com>
2019-12-09 20:42:03 -0400

    Enable PWA

--- src/index.tsx
+++ src/index.tsx
@@ -25,4 +25,4 @@ render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
